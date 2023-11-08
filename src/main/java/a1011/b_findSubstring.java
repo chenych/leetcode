fd8eddb2632984854e1c99b5ebb23670f0c9ceb6
yuchong.cyc <kkk1986521@163.com>
2023-11-08 10:35:53 +0800

    1106 复习历史

--- src/main/java/a1011/b_findSubstring.java
+++ src/main/java/a1011/b_findSubstring.java
@@ -1,5 +1,7 @@
 package a1011;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -8,7 +10,8 @@ import java.util.List;
  * 给定一个字符串 s 和一个字符串数组 words。 words 中所有字符串 长度相同。
  * s 中的 串联子串 是指一个包含  words 中所有字符串以任意顺序排列连接起来的子串。
  * <p>
- * 例如，如果 words = ["ab","cd","ef"]， 那么 "abcdef"， "abefcd"，"cdabef"， "cdefab"，"efabcd"， 和 "efcdab" 都是串联子串。 "acdbef" 不是串联子串，因为他不是任何 words 排列的连接。
+ * 例如，如果 words = ["ab","cd","ef"]， 那么 "abcdef"， "abefcd"，"cdabef"， "cdefab"，"efabcd"， 和 "efcdab" 都是串联子串。 "acdbef" 不是串联子串，
+ * 因为他不是任何 words 排列的连接。
  * 返回所有串联子串在 s 中的开始索引。你可以以 任意顺序 返回答案。
  * <p>
  * 示例 1：
@@ -45,7 +48,44 @@ import java.util.List;
  */
 public class b_findSubstring {
     public List<Integer> findSubstring(String s, String[] words) {
-
-        return null;
+        List<Integer> res = new ArrayList<Integer>();
+        int wordNum = words.length;
+        if (wordNum == 0) {
+            return res;
+        }
+        int wordLen = words[0].length();
+        //HashMap1 存所有单词
+        HashMap<String, Integer> allWords = new HashMap<String, Integer>();
+        for (String w : words) {
+            int value = allWords.getOrDefault(w, 0);
+            allWords.put(w, value + 1);
+        }
+        //遍历所有子串
+        for (int i = 0; i < s.length() - wordNum * wordLen + 1; i++) {
+            //HashMap2 存当前扫描的字符串含有的单词
+            HashMap<String, Integer> hasWords = new HashMap<String, Integer>();
+            int num = 0;
+            //判断该子串是否符合
+            while (num < wordNum) {
+                String word = s.substring(i + num * wordLen, i + (num + 1) * wordLen);
+                //判断该单词在 HashMap1 中
+                if (allWords.containsKey(word)) {
+                    int value = hasWords.getOrDefault(word, 0);
+                    hasWords.put(word, value + 1);
+                    //判断当前单词的 value 和 HashMap1 中该单词的 value
+                    if (hasWords.get(word) > allWords.get(word)) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                num++;
+            }
+            //判断是不是所有的单词都符合条件
+            if (num == wordNum) {
+                res.add(i);
+            }
+        }
+        return res;
     }
 }
