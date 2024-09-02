e1daeae77c5461306614e15f071f5deb9ad22c2c
冲达 <yuchong.cyc@alibaba-inc.com>
2024-09-02 16:26:53 +0800

    hot

---
+++ src/main/java/hot100/c_longestConsecutive.java
@@ -0,0 +1,56 @@
+package hot100;
+
+import com.sun.xml.internal.ws.addressing.WsaTubeHelperImpl;
+
+import java.util.*;
+
+/**
+ * 128. 最长连续序列
+ * 中等
+ * 相关标签
+ * 相关企业
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * <p>
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+ * 输出：9
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ */
+public class c_longestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int maxLen = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int len = 0;
+                while (set.contains(num++)) {
+                    len++;
+                }
+                maxLen = Math.max(maxLen, len);
+            }
+        }
+        return maxLen;
+    }
+
+
+    public static void main(String[] args) {
+        int[] num = {100, 4, 200, 1, 3, 2};
+        new c_longestConsecutive().longestConsecutive(num);
+    }
+}
