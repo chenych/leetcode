775dc16968771a467890620d0fde0f3ab518866b
冲达 <yuchong.cyc@alibaba-inc.com>
2024-09-10 10:49:10 +0800

    hot

--- src/main/java/hot100/e_maxArea.java
+++ src/main/java/hot100/e_maxArea.java
@@ -41,7 +41,7 @@ public class e_maxArea {
         int ans = 0;
         int i = 0, j = height.length - 1;
         while (i < j) {
-            int area = Math.min(height[i], height[j] * (j - i));
+            int area = Math.min(height[i], height[j]) * (j - i);
             ans = Math.max(ans, area);
             if (height[i] <= height[j]) {
                 ++i;
