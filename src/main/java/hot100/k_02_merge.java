15905a37e835c4145b59777d569243fb4b10aa08
冲达 <yuchong.cyc@alibaba-inc.com>
2024-10-14 17:48:36 +0800

    hot

---
+++ src/main/java/hot100/k_02_merge.java
@@ -0,0 +1,50 @@
+package hot100;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 56. 合并区间
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+ * 请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+ * 示例 1：
+ * <p>
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 示例 2：
+ * <p>
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 104
+ */
+public class k_02_merge {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+        int n = intervals.length;
+        int right = 0;
+        List<int[]> ranges = new ArrayList<>();
+        while (right < n) {
+            int start = intervals[right][0];
+            int end = intervals[right][1];
+            int left = right + 1;
+            while (left < n && intervals[left][0] <= end) {
+                end = Math.max(end, intervals[left][1]);
+                left++;
+            }
+            ranges.add(new int[]{start, end});
+            right = left;
+        }
+        return ranges.toArray(new int[ranges.size()][]);
+    }
+}
---
+++ src/main/java/hot100/k_03_rotate.java
@@ -0,0 +1,44 @@
+package hot100;
+
+import java.util.*;
+
+/**
+ * 189. 轮转数组
+ * 中等
+ * 相关标签
+ * 相关企业
+ * 提示
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右轮转 1 步: [7,1,2,3,4,5,6]
+ * 向右轮转 2 步: [6,7,1,2,3,4,5]
+ * 向右轮转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ * <p>
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右轮转 1 步: [99,-1,-100,3]
+ * 向右轮转 2 步: [3,99,-1,-100]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 105
+ * -231 <= nums[i] <= 231 - 1
+ * 0 <= k <= 105
+ */
+public class k_03_rotate {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newarr = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            newarr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newarr, 0, nums, 0, n);
+    }
+}
